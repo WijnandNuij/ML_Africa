22a4193176b3316abccc4eb9327d2ec367280964
Wijnand Nuij <wijnandnuij@gmail.com>
2014-09-13 15:56:24 +0200

    h2o attempt

--- africa_h2o.R
+++ africa_h2o.R
@@ -25,6 +25,8 @@ run <- function(dir = '/home/wijnand/R_workspace_africa')
         trainPart_h2o <- as.h2o(localH2O, trainPart, header = T)
         testPart_h2o <- as.h2o(localH2O, testPart, header = T)
         
+        #predictColumns <- c("Ca", "P", "pH", "SOC", "Sand")
+        predictColumns <- c("P")
         trainColumns <- colnames(trainPart[, !colnames(trainPart) %in% c("PIDN", "Ca", "P", "pH", "SOC", "Sand")])
         
         #trainedModel <- h2o.glm(x = trainColumns,
@@ -32,18 +34,34 @@ run <- function(dir = '/home/wijnand/R_workspace_africa')
         #                        data = trainPart_h2o,
         #                        family = "gaussian")
         
-        trainedModel <- h2o.gbm(x = trainColumns,
-                                y = "Ca",
-                                data = trainPart_h2o,
-                                distribution = "gaussian",
-                                n.trees = 400)
-        
-        prediction <- h2o.predict(trainedModel, newdata = testPart_h2o)
-        predictionFrame <- as.data.frame(prediction)
+        predictions <- as.data.frame(testPart$PIDN)
+        RMSE <- NULL
         
         library(Metrics)
-        print(rmse(predictionFrame, testPart$Ca))
-        
+        for(predictor in predictColumns)
+        {
+                trainedModel <- h2o.gbm(x = trainColumns,
+                                        y = predictor,
+                                        data = trainPart_h2o,
+                                        distribution = "gaussian",
+                                        n.trees = 250,
+                                        importance = T,
+                                        shrinkage = 0.05,
+                                        nfolds = 10
+                                        )
+                
+                prediction <- h2o.predict(trainedModel, newdata = testPart_h2o)
+                predictionFrame <- as.data.frame(prediction)
+                
+                predictions[predictor] <- predictionFrame
+                RMSE <- c(RMSE, rmse(predictionFrame, testPart[predictor]))              
+                print(rmse(predictionFrame, testPart[predictor]))
+        }
         h2o.shutdown(localH2O, prompt = F)
+        write.csv(predictions, paste0(dir, '/resources/result.csv'), quote=F, row.names=F)
+        
+        print("===summary===")
+        print(predictColumns)
+        print(RMSE)
+        print(paste0("MCRMSE (mean columnwise root mean squared error): ", sum(RMSE) / 5))
 }
-
