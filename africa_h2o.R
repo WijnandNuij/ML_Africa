bb593a6ec526bd2dc7b4d429bdf4839c75b13234
Wijnand Nuij <wijnandnuij@gmail.com>
2014-09-13 11:57:31 +0200

    h2o attempt

--- africa_h2o.R
+++ africa_h2o.R
@@ -1,31 +1,49 @@
-# predict:
-# Ca
-# P
-# pH
-# SOC
-# Sand
+# predict: Ca, P, pH, SOC, Sand
 
 run <- function(dir = '/home/wijnand/R_workspace_africa')
 {
-        dir <- '/home/wijnand/R_workspace_africa'
-        training <- read.csv(paste0(dir, '/resources/train.csv'), header = T)
+        ## delete h2o tmp
+        system("killall -9 java")
+        system("rm -rf /tmp/h2o-wijnand/*")
+        ## start h2o
+        system("nohup java -jar -Xmx2g /home/wijnand/h2o-2.6.0.11/h2o.jar > /home/wijnand/h2o-2.6.0.11/run.log &")
+        print('starting h2o...')
+        Sys.sleep(2)
         
-        # identifier
-        PIDN <- training$PIDN
-        training$PIDN <- NULL
+        set.seed(1)
+        training <- read.csv(paste0(dir, '/resources/train.csv'), header = T)
         
         library(h2o)
         localH2O <- h2o.init()
         print(localH2O)
         
-        dat_h2o <- as.h2o(localH2O, training, header = T)
+        library(caret)
+        number <- createDataPartition(y = training$Ca, p = .8, list = F)
+        trainPart <- training[number,]
+        testPart <- training[-number,]
         
-        trainingColumns <- colnames(training[, !colnames(training) %in% c("Ca", "P", "pH", "SOC", "Sand")])
+        trainPart_h2o <- as.h2o(localH2O, trainPart, header = T)
+        testPart_h2o <- as.h2o(localH2O, testPart, header = T)
         
-        trainedModel <- h2o.glm(x = trainingColumns,
+        trainColumns <- colnames(trainPart[, !colnames(trainPart) %in% c("PIDN", "Ca", "P", "pH", "SOC", "Sand")])
+        
+        #trainedModel <- h2o.glm(x = trainColumns,
+        #                        y = "Ca",
+        #                        data = trainPart_h2o,
+        #                        family = "gaussian")
+        
+        trainedModel <- h2o.gbm(x = trainColumns,
                                 y = "Ca",
-                                data = dat_h2o,
-                                family = "gaussian")
+                                data = trainPart_h2o,
+                                distribution = "gaussian",
+                                n.trees = 400)
         
-        h2o.shutdown(localH2O)
+        prediction <- h2o.predict(trainedModel, newdata = testPart_h2o)
+        predictionFrame <- as.data.frame(prediction)
+        
+        library(Metrics)
+        print(rmse(predictionFrame, testPart$Ca))
+        
+        h2o.shutdown(localH2O, prompt = F)
 }
+
